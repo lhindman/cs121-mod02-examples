473547256bedad6cb7503b29e40e0b40f742b5e9
Luke Hindman <luke@platypuscreations.net>
2020-09-02 09:44:11 -0700

    Initial commit

---
+++ MyApp/MyApp.java
@@ -0,0 +1,10 @@
+/**
+ * Sample application
+ * @author Luke Hindman
+ **/
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to MyApp!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# basic-java-workspace
+This is a template for creating a basic Java workspace
